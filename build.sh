bbb7a5e794e4e3c8269ea7bfd8b668fe365dd0e2
YooJia.Chen <yoojiachen@gmail.com>
2019-06-30 18:51:48 +0800

    Update supports

--- build.sh
+++ build.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-modules=( \
-"endpoint" \
-"trigger" \
-)
+modules=( "endpoint" "trigger" )
 
 makeModule() {
     for dir in ${modules[@]} ; do
         echo "###### Building module: ${dir}"
+        # Into a module dir
         cd ${dir}
-        # Build linux dist
         GOOS=linux GOARCH=arm make -f ../Makefile $*
         GOOS=linux GOARCH=amd64 make -f ../Makefile $*
+        GOOS=linux make -f ../Makefile manifest
         cd -
     done
 }
 
-makeModule $*
+makeModule image push
