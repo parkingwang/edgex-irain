f5c9c695cf9b19b4cd6b3bc0c9e5aaa2367eb3a8
陈哈哈 <yoojiachen@gmail.com>
2019-08-20 14:39:36 +0800

    Update dep and build script

--- build.sh
+++ build.sh
@@ -2,17 +2,22 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
+SUDO="sudo "
+if [ "Darwin" == "$(uname -s)" ]; then
+    SUDO=""
+fi
+
 modules=( "node" )
 
 makeModule() {
     for dir in ${modules[@]} ; do
         echo "###### Building module: ${dir}"
-        # Into a module dir
         cd ${dir}
-        GOOS=linux GOARCH=arm make -f ../Makefile $*
-        GOOS=linux GOARCH=arm64 make -f ../Makefile $*
-        GOOS=linux GOARCH=amd64 make -f ../Makefile $*
-        GOOS=linux make -f ../Makefile manifest
+        # Build linux dist
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=arm make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=arm64 make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=amd64 make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux make -f ../Makefile manifest
         cd -
     done
 }
